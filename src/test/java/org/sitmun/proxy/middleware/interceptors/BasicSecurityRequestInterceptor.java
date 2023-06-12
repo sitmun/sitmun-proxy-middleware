e6ab92eb9b81110c08b17977736d90b674763ce1
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2023-06-12 15:18:34 +0200

    Code format

--- src/test/java/org/sitmun/proxy/middleware/interceptors/BasicSecurityRequestInterceptor.java
+++ src/test/java/org/sitmun/proxy/middleware/interceptors/BasicSecurityRequestInterceptor.java
@@ -1,32 +1,31 @@
 package org.sitmun.proxy.middleware.interceptors;
 
-import java.io.IOException;
-
-import okhttp3.Response;
-import okhttp3.ResponseBody;
-
-import java.util.Base64;
-
 import okhttp3.MediaType;
 import okhttp3.Request;
-
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Component
 public class BasicSecurityRequestInterceptor implements TestInterceptor {
 
-	@Override
-	public Response intercept(Chain arg0) throws IOException {
-		Request request = arg0.request();
-		String authorization = request.header("Authorization");
-		Response response = arg0.proceed(request);
-		if (authorization != null && authorization.startsWith("Basic")) {
-			String token = decodeAuthorization(authorization);
-			ResponseBody body = ResponseBody.create(token, MediaType.get("application/json"));
-			response = response.newBuilder().body(body).code(200).build();
-		}
-		return response;
+  @NotNull
+  @Override
+  public Response intercept(Chain arg0) throws IOException {
+    Request request = arg0.request();
+    String authorization = request.header("Authorization");
+    Response response = arg0.proceed(request);
+    if (authorization != null && authorization.startsWith("Basic")) {
+      String token = decodeAuthorization(authorization);
+      ResponseBody body = ResponseBody.create(token, MediaType.get("application/json"));
+      response = response.newBuilder().body(body).code(200).build();
+    }
+    return response;
 	}
 
 	private String decodeAuthorization(String authorization) {
